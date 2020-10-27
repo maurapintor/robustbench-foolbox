f0945180c3f54e9c0584f4e35db1de8a22488ce9
maurapintor <maura.pintor@gmail.com>
2020-10-27 17:41:00 +0100

    fix: map location

--- foolbox_robustbench.py
+++ foolbox_robustbench.py
@@ -3,8 +3,8 @@ import foolbox as fb
 from load_model import load_model_to_device
 
 
-def create(model_name, norm):
-    model = load_model_to_device(model_name=model_name, norm=norm, device='cpu')
+def create(model_name, norm, device='cpu'):
+    model = load_model_to_device(model_name=model_name, norm=norm, device=device)
     model.eval()
 
     fmodel = fb.models.PyTorchModel(model, bounds=(0, 1))
